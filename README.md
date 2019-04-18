15ce869c1023fd71a55674243e8445240475134f
Matjaž Mav <matjaz.mav@gmail.com>
2019-04-18 16:11:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Particles Filter Tracking
+
+Faculty of Computer and Information Science, University of Ljubljana, Masters Program, Advanced Methods of Computer Vision, Seminar 04
+
+## Description
+
+TODO
---
+++ startup.m
@@ -0,0 +1,6 @@
+addpath('./helpers');
+addpath('./implementations');
+
+set(groot, 'defaulttextinterpreter',            'None');  
+set(groot, 'defaultAxesTickLabelInterpreter',   'None');  
+set(groot, 'defaultLegendInterpreter',          'None');
